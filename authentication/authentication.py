3fec69b5b5b7885677f18f792316cdde91f09166
omkar parmar <omkar.parmar@encoresky.com>
2024-12-27 20:17:48 +0530

    incomplete changes for oauth login API

---
+++ authentication/authentication.py
@@ -0,0 +1,23 @@
+from common_app.models import User
+from django.contrib.auth.backends import BaseBackend
+
+class EmailBackend(BaseBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        """
+        Authenticate the user by email and password.
+        """
+        try:
+            user = User.objects.get(email=username)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        """
+        Return the user object by user ID.
+        """
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
---
+++ common_app/management/commands/commands.py
@@ -0,0 +1,19 @@
+import uuid
+from common_app.models import OAuthApplication
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = "Create OAuth application for the project"
+
+    def handle(self, *args, **kwargs):
+        OAuthApplication.objects.create(
+            name="mmp_app",
+            client_id=str(uuid.uuid4()),
+            client_secret=str(uuid.uuid4()),
+            redirect_uris="http://localhost:8000/oauth/callback/",
+            # client_type="confidential",
+            # authorization_grant_type="authorization_code",
+            # scope="read write",
+            # is_active=True
+        )
+        self.stdout.write(self.style.SUCCESS("OAuth application created successfully"))
